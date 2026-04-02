0c5e4ac88cca0940cc89dc6e2d4d5774983e2e6f
gh9727 <gj9727@gmail.com>
2026-04-02 14:08:58 +0900

    keycloak 로컬 설정 및 관련 의존성 추가

---
+++ src/main/java/com/loopang/gateway/config/UserContextFilter.java
@@ -0,0 +1,82 @@
+package com.loopang.gateway.config;
+
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class UserContextFilter implements GlobalFilter, Ordered {
+
+//  private final Tracer tracer;
+//  private static final String HEADER_TRACE_ID = "X-Trace-Id";
+  private static final String HEADER_USER_UUID = "X-User-UUID";
+  private static final String HEADER_EMAIL = "X-User-Email";
+  private static final String HEADER_USER_NAME = "X-User-Name";
+  private static final String HEADER_SLACK_ID = "X-User-Slack-Id";
+  private static final String HEADER_ROLE = "X-User-Roles";
+  private static final String HEADER_ENABLED = "X-User-Enabled"; // 배열로 들어오는 String 처리
+
+
+  public UserContextFilter() {
+//    this.tracer = tracer;
+  }
+
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    ServerHttpRequest cleanedRequest = exchange.getRequest().mutate()
+        .headers(httpHeaders -> {
+          httpHeaders.remove(HEADER_USER_UUID);
+          httpHeaders.remove(HEADER_EMAIL);
+          httpHeaders.remove(HEADER_USER_NAME);
+          httpHeaders.remove(HEADER_SLACK_ID);
+          httpHeaders.remove(HEADER_ROLE);
+          httpHeaders.remove(HEADER_ENABLED);
+        })
+//        .header(HEADER_TRACE_ID, traceId)
+        .build();
+    ServerWebExchange mutatedExchange  = exchange.mutate().request(cleanedRequest).build();
+
+    return exchange.getPrincipal()
+        .cast(JwtAuthenticationToken.class)
+        .map(jwtAuth -> {
+          // 1. Keycloak 토큰 클레임 추출
+          Map<String, Object> claims = jwtAuth.getTokenAttributes();
+          String id = (String) claims.get("sub");
+          String email = (String) claims.get("email");
+          String name = (String) claims.get("name");
+          String slackId = (String) claims.get("slack_id");
+          String role = (String) claims.get("role");
+          String enabled = (String) claims.get("is_enabled");
+
+          log.debug("Global Filter - User Authenticated: {}", id);
+
+          // 헤더 UserData 추가 (기존 헤더 초기화 후 신규 설정)
+          ServerHttpRequest request  = mutatedExchange.getRequest().mutate()
+              .header(HEADER_USER_UUID, id != null ? id : "")
+              .header(HEADER_EMAIL, email != null ? email : "")
+              .header(HEADER_USER_NAME, name != null ? name : "")
+              .header(HEADER_SLACK_ID, slackId != null ? slackId : "")
+              .header(HEADER_ROLE, role != null ? role : "")
+              .header(HEADER_ENABLED, enabled != null ? enabled : "")
+              .build();
+
+          return exchange.mutate().request(request).build();
+        })
+        .defaultIfEmpty(mutatedExchange) // 인증 안 된 경우(비로그인) 그대로 전달하여 App에서 null 처리 유도
+        .flatMap(chain::filter);
+  }
+
+  @Override
+  public int getOrder() {
+    return SecurityWebFiltersOrder.AUTHORIZATION.getOrder() + 1;
+  }
+}
